f93a810551f02e87a778a0e574a233703ec4e894
Dylan Reitsma <dylreitsma@gmail.com>
2023-04-18 10:50:04 -0400

    First commit

---
+++ src/App.java
@@ -0,0 +1,12 @@
+import java.math.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        BigDecimal a = new BigDecimal(1);
+
+        
+        
+    }
+}
